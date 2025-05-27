167b02dcd605d86c50dacfb4f34a67f345216f23
Akshat-NegI27 <141574274+Akshat-NegI27@users.noreply.github.com>
2025-05-27 23:17:14 +0530

    Gallery Update✅

--- src/components/gallery-content/galleryContent.jsx
+++ src/components/gallery-content/galleryContent.jsx
@@ -6,6 +6,20 @@ import { FaExpandAlt } from "react-icons/fa";
 const images = [
   { src: "https://i.ibb.co/sdkXf24q/IMG-20240106-173916-532.webp" },
   { src: "https://i.ibb.co/TDcc4X55/IMG-20240106-173916-662.webp" },
+  { src: "https://i.ibb.co/sdkXf24q/IMG-20240106-173916-532.webp" },
+  { src: "https://i.ibb.co/TDcc4X55/IMG-20240106-173916-662.webp" },
+  { src: "https://i.ibb.co/sdkXf24q/IMG-20240106-173916-532.webp" },
+  { src: "https://i.ibb.co/TDcc4X55/IMG-20240106-173916-662.webp" },
+  { src: "https://i.ibb.co/sdkXf24q/IMG-20240106-173916-532.webp" },
+  { src: "https://i.ibb.co/TDcc4X55/IMG-20240106-173916-662.webp" },
+  { src: "https://i.ibb.co/sdkXf24q/IMG-20240106-173916-532.webp" },
+  { src: "https://i.ibb.co/TDcc4X55/IMG-20240106-173916-662.webp" },
+  { src: "https://i.ibb.co/sdkXf24q/IMG-20240106-173916-532.webp" },
+  { src: "https://i.ibb.co/TDcc4X55/IMG-20240106-173916-662.webp" },
+  { src: "https://i.ibb.co/sdkXf24q/IMG-20240106-173916-532.webp" },
+  { src: "https://i.ibb.co/TDcc4X55/IMG-20240106-173916-662.webp" },
+  { src: "https://i.ibb.co/sdkXf24q/IMG-20240106-173916-532.webp" },
+  { src: "https://i.ibb.co/TDcc4X55/IMG-20240106-173916-662.webp" },
 ];
 
 const GalleryContent = () => {
@@ -21,6 +35,11 @@ const GalleryContent = () => {
 
   return (
     <div className="gallery-container">
+      <div className="gallery-header">
+        <h1>The Gallery</h1>
+        <h2>Echoes of Nature</h2>
+        <p>Captured moments that reflect our mission in motion.</p>
+      </div>
       <h1 className="gallery-title">Explore the Gallery</h1>
       <div className="masonry-grid">
         {images.map((img, idx) => (
