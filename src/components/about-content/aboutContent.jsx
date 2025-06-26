21b259204e161cfc488a81845aba9a2cb1eb6d72
Advik <advikbhatt24@gmail.com>
2025-06-26 17:17:00 +0530

    data updated ✅

--- src/components/about-content/aboutContent.jsx
+++ src/components/about-content/aboutContent.jsx
@@ -141,16 +141,34 @@ const AboutContent = () => {
       </section>
 
       <section className="mentor-highlight">
+        <section className="what-we-doo">
+          <h2>
+            OUR <span className="highlight">MENTORS</span>
+
+          </h2>        <p className="description">
+            Our mentors are experts in their fields, guiding our initiatives
+            with their extensive knowledge and experience. They play a crucial
+            role in shaping our programs and ensuring their effectiveness.
+
+
+          </p></section>
+
         <div className="mentor-container">
           <div className="mentor-image">
-            <img src="/assets/img/kusum.jpeg" alt="Dr. Kusum" />
+            <img src="/assets/img/amit.jpeg" alt="Dr. Kusum" />
           </div>
+
           <div className="mentor-text">
-                        <h2>Meet Our Mentor</h2>
 
             <h3>Dr. Amit Kumar</h3>
             <p>
-              Dr. Kusum Arunachalam, a distinguished scholar with over 27 years of experience, specializes in Restoration Ecology and Natural Resource Management, focusing on the Indian Himalayas. A tropical soil ecologist, she earned her Ph.D. from NEHU, India. She received the CSIR-Independent Research Associateship (1997) and DST-Young Scientist Award (1998) before joining NERIST as a Lecturer, later becoming an Assistant Professor (2007). She also served as a Senior Scientific Officer at the Uttarakhand State Council for Science & Technology (2007-2009). Currently, she is a Professor at Doon University’s School of Environment & Natural Resources, where she has also held key administrative positions, including Dean Academics, Dean of the School, Director-IQAC, and Acting Vice Chancellor. Dr. Arunachalam has led major research projects on soil ecology, wetland conservation, and ecosystem dynamics, collaborating with indigenous communities like the Jad Bhutiya, Jaunsari, Apatani, and Nishi to validate their traditional knowledge. She has completed 15 R&D projects worth ₹10 crores, funded by agencies like UGC, DST, CSIR, MoEF&CC, ICAR, GIZ, and MoES, and has contributed to national task forces on Himalayan sustainability. Recognized for her contributions, she received the National Young Woman Bioscientist Award (2005) and the BOYSCAST Fellowship (2002-03) for research in Germany. A Fellow of prestigious organizations like the International Society of Tropical Ecology and the National Environmental Science Academy, she has mentored numerous Master’s, Doctoral, and Post-Doctoral students. Through her multidisciplinary approach—integrating modern science with traditional ecological wisdom—she has significantly advanced ecological research, empowered indigenous communities, and promoted sustainable environmental practices.            </p>
+              Since a young age, I've been fascinated by plants, wanting to know their names, how and where they grow, and how they differed from one another. With time, my fascination grew into a desire to learn more about plants, which led me to pursue a bachelor's degree in forestry from the University of Horticulture and Forestry in Himachal Pradesh, as well as M.Sc. and Ph.D. degrees in forestry from the Forest Research Institute University, Dehradun.
+
+              I had the privilege of investigating the flora and ecology of the cold-arid and Trans-Himalayan zones of Nanda Devi Biosphere Reserve for my doctoral thesis. Delineation of a new biogeographic province in the Trans-Himalaya, designation of Caragana versicolor as a keystone species, flora of NDBR and discovery of few new species in one of the least explored regions of the Western Himalaya were few outcomes of the thesis.
+
+              I joined Wildlife Institute of India (WII) as a faculty in May, 2018. Prior to joining WII, I had the honour of teaching Systematic Botany and Forest Botany as a faculty at Forest Research Institute University, Dehradun and as a visiting faculty at Indira Gandhi National Forest Academy, Dehradun.
+
+              Plant ecology and field botany are the main focuses of my research. The current research includes understanding vegetation patterns, plant associations, floristic diversity assessment, ethnobotany, and the threatened medicinal plants conservation in the Western Himalaya. I enjoy photographing plants, hiking, biking and playing table tennis.     </p>
           </div>
         </div>
       </section>      <section className="mentor-highlight">
