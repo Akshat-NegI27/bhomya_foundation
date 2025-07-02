e1083536b45fde6eb69f08e8460fe0979e742279
Akshat-NegI27 <141574274+Akshat-NegI27@users.noreply.github.com>
2025-07-02 12:57:07 +0530

    Css Fixed✅

--- src/components/about-content/aboutContent.jsx
+++ src/components/about-content/aboutContent.jsx
@@ -20,7 +20,7 @@ const AboutContent = () => {
       end: "+=1280 top",
       pin: pinWrap,
       pinSpacing: false,
-      scrub: true,
+      scrub: 2,
     });
 
     return () => {
@@ -82,7 +82,6 @@ const AboutContent = () => {
           </div>
         </div>
       </section>
-
       {/* What We Do */}
       <section className="what-we-do">
         <h2>
@@ -139,19 +138,17 @@ const AboutContent = () => {
           </div>
         </div>
       </section>
-
       <section className="mentor-highlight">
         <section className="what-we-doo">
           <h2>
             OUR <span className="highlight">MENTORS</span>
-
-          </h2>        <p className="description">
+          </h2>{" "}
+          <p className="description">
             Our mentors are experts in their fields, guiding our initiatives
             with their extensive knowledge and experience. They play a crucial
             role in shaping our programs and ensuring their effectiveness.
-
-
-          </p></section>
+          </p>
+        </section>
 
         <div className="mentor-container">
           <div className="mentor-image">
@@ -159,19 +156,37 @@ const AboutContent = () => {
           </div>
 
           <div className="mentor-text">
-
             <h3>Dr. Amit Kumar</h3>
             <p>
-              Since a young age, I've been fascinated by plants, wanting to know their names, how and where they grow, and how they differed from one another. With time, my fascination grew into a desire to learn more about plants, which led me to pursue a bachelor's degree in forestry from the University of Horticulture and Forestry in Himachal Pradesh, as well as M.Sc. and Ph.D. degrees in forestry from the Forest Research Institute University, Dehradun.
-
-              I had the privilege of investigating the flora and ecology of the cold-arid and Trans-Himalayan zones of Nanda Devi Biosphere Reserve for my doctoral thesis. Delineation of a new biogeographic province in the Trans-Himalaya, designation of Caragana versicolor as a keystone species, flora of NDBR and discovery of few new species in one of the least explored regions of the Western Himalaya were few outcomes of the thesis.
-
-              I joined Wildlife Institute of India (WII) as a faculty in May, 2018. Prior to joining WII, I had the honour of teaching Systematic Botany and Forest Botany as a faculty at Forest Research Institute University, Dehradun and as a visiting faculty at Indira Gandhi National Forest Academy, Dehradun.
-
-              Plant ecology and field botany are the main focuses of my research. The current research includes understanding vegetation patterns, plant associations, floristic diversity assessment, ethnobotany, and the threatened medicinal plants conservation in the Western Himalaya. I enjoy photographing plants, hiking, biking and playing table tennis.     </p>
+              Since a young age, I've been fascinated by plants, wanting to know
+              their names, how and where they grow, and how they differed from
+              one another. With time, my fascination grew into a desire to learn
+              more about plants, which led me to pursue a bachelor's degree in
+              forestry from the University of Horticulture and Forestry in
+              Himachal Pradesh, as well as M.Sc. and Ph.D. degrees in forestry
+              from the Forest Research Institute University, Dehradun. I had the
+              privilege of investigating the flora and ecology of the cold-arid
+              and Trans-Himalayan zones of Nanda Devi Biosphere Reserve for my
+              doctoral thesis. Delineation of a new biogeographic province in
+              the Trans-Himalaya, designation of Caragana versicolor as a
+              keystone species, flora of NDBR and discovery of few new species
+              in one of the least explored regions of the Western Himalaya were
+              few outcomes of the thesis. I joined Wildlife Institute of India
+              (WII) as a faculty in May, 2018. Prior to joining WII, I had the
+              honour of teaching Systematic Botany and Forest Botany as a
+              faculty at Forest Research Institute University, Dehradun and as a
+              visiting faculty at Indira Gandhi National Forest Academy,
+              Dehradun. Plant ecology and field botany are the main focuses of
+              my research. The current research includes understanding
+              vegetation patterns, plant associations, floristic diversity
+              assessment, ethnobotany, and the threatened medicinal plants
+              conservation in the Western Himalaya. I enjoy photographing
+              plants, hiking, biking and playing table tennis.{" "}
+            </p>
           </div>
         </div>
-      </section>      <section className="mentor-highlight">
+      </section>{" "}
+      <section className="mentor-highlight">
         <div className="mentor-container">
           <div className="mentor-image">
             <img src="/assets/img/kusum.jpeg" alt="Dr. Kusum" />
@@ -179,7 +194,37 @@ const AboutContent = () => {
           <div className="mentor-text">
             <h3>Dr. Kusum Arunachalam</h3>
             <p>
-              Dr. Kusum Arunachalam, a distinguished scholar with over 27 years of experience, specializes in Restoration Ecology and Natural Resource Management, focusing on the Indian Himalayas. A tropical soil ecologist, she earned her Ph.D. from NEHU, India. She received the CSIR-Independent Research Associateship (1997) and DST-Young Scientist Award (1998) before joining NERIST as a Lecturer, later becoming an Assistant Professor (2007). She also served as a Senior Scientific Officer at the Uttarakhand State Council for Science & Technology (2007-2009). Currently, she is a Professor at Doon University’s School of Environment & Natural Resources, where she has also held key administrative positions, including Dean Academics, Dean of the School, Director-IQAC, and Acting Vice Chancellor. Dr. Arunachalam has led major research projects on soil ecology, wetland conservation, and ecosystem dynamics, collaborating with indigenous communities like the Jad Bhutiya, Jaunsari, Apatani, and Nishi to validate their traditional knowledge. She has completed 15 R&D projects worth ₹10 crores, funded by agencies like UGC, DST, CSIR, MoEF&CC, ICAR, GIZ, and MoES, and has contributed to national task forces on Himalayan sustainability. Recognized for her contributions, she received the National Young Woman Bioscientist Award (2005) and the BOYSCAST Fellowship (2002-03) for research in Germany. A Fellow of prestigious organizations like the International Society of Tropical Ecology and the National Environmental Science Academy, she has mentored numerous Master’s, Doctoral, and Post-Doctoral students. Through her multidisciplinary approach—integrating modern science with traditional ecological wisdom—she has significantly advanced ecological research, empowered indigenous communities, and promoted sustainable environmental practices.            </p>
+              Dr. Kusum Arunachalam, a distinguished scholar with over 27 years
+              of experience, specializes in Restoration Ecology and Natural
+              Resource Management, focusing on the Indian Himalayas. A tropical
+              soil ecologist, she earned her Ph.D. from NEHU, India. She
+              received the CSIR-Independent Research Associateship (1997) and
+              DST-Young Scientist Award (1998) before joining NERIST as a
+              Lecturer, later becoming an Assistant Professor (2007). She also
+              served as a Senior Scientific Officer at the Uttarakhand State
+              Council for Science & Technology (2007-2009). Currently, she is a
+              Professor at Doon University’s School of Environment & Natural
+              Resources, where she has also held key administrative positions,
+              including Dean Academics, Dean of the School, Director-IQAC, and
+              Acting Vice Chancellor. Dr. Arunachalam has led major research
+              projects on soil ecology, wetland conservation, and ecosystem
+              dynamics, collaborating with indigenous communities like the Jad
+              Bhutiya, Jaunsari, Apatani, and Nishi to validate their
+              traditional knowledge. She has completed 15 R&D projects worth ₹10
+              crores, funded by agencies like UGC, DST, CSIR, MoEF&CC, ICAR,
+              GIZ, and MoES, and has contributed to national task forces on
+              Himalayan sustainability. Recognized for her contributions, she
+              received the National Young Woman Bioscientist Award (2005) and
+              the BOYSCAST Fellowship (2002-03) for research in Germany. A
+              Fellow of prestigious organizations like the International Society
+              of Tropical Ecology and the National Environmental Science
+              Academy, she has mentored numerous Master’s, Doctoral, and
+              Post-Doctoral students. Through her multidisciplinary
+              approach—integrating modern science with traditional ecological
+              wisdom—she has significantly advanced ecological research,
+              empowered indigenous communities, and promoted sustainable
+              environmental practices.{" "}
+            </p>
           </div>
         </div>
       </section>
@@ -191,13 +236,39 @@ const AboutContent = () => {
           <div className="mentor-text">
             <h3>Dr. Ujjwal Kumar</h3>
             <p>
-              Dr. Ujjwal Kumar has more than 18 years of research and teaching experience in the field of Atmospheric science/Environmental Science/Atmospheric-Environmental Modelling / Statistical-Time Series modelling / machine learning analysis of environmental data. He is currently serving at School of Envrionment & Natural Resources, Doon University, India. Prior to joining Doon University, he worked as Post Doc Researcher at KNMI-Royal Netherlands Meteorological Institute, Netherlands for three and half years  mainly on operational air quality forecasting system using Chemical Transport Models with data assimilation of surface/satellite observations. Prior to this, he also worked for two years at VITO-Flemish Institute for Technological Research, Belgium on data assimilation of chemical transport model AURORA. He has more than 40 research publications to his credit in different high quality impact factor journals including Nature, Atmospheric Chemistry & Physics (ACP), and Atmospheric Environment among others. He has carried out several scientific/action-oriented projects as Principal Investigator (PI) sponsored by DST, SERB, Ministry of Environment Forest & Climate Change, UGC and has been a collaborator in several international projects such as MACC (a EU project), PASODOBLE (a EU project), SMOGPROG (Ministry of Environment, Netherlands). He has been a recipient of Young Scientist medal by Society for Science of Climate Change and Sustainable Environment for exemplary work in Environment & Sustainable Development (2023), Uttarakhand Governor’s Award for 2nd best Research Publications in Science (2015-16).  He also serves as an editorial board member of the journal SN Applied Sciences, Discover Forests and Journal of Aritifical Intelligence.
+              Dr. Ujjwal Kumar has more than 18 years of research and teaching
+              experience in the field of Atmospheric science/Environmental
+              Science/Atmospheric-Environmental Modelling / Statistical-Time
+              Series modelling / machine learning analysis of environmental
+              data. He is currently serving at School of Envrionment & Natural
+              Resources, Doon University, India. Prior to joining Doon
+              University, he worked as Post Doc Researcher at KNMI-Royal
+              Netherlands Meteorological Institute, Netherlands for three and
+              half years mainly on operational air quality forecasting system
+              using Chemical Transport Models with data assimilation of
+              surface/satellite observations. Prior to this, he also worked for
+              two years at VITO-Flemish Institute for Technological Research,
+              Belgium on data assimilation of chemical transport model AURORA.
+              He has more than 40 research publications to his credit in
+              different high quality impact factor journals including Nature,
+              Atmospheric Chemistry & Physics (ACP), and Atmospheric Environment
+              among others. He has carried out several
+              scientific/action-oriented projects as Principal Investigator (PI)
+              sponsored by DST, SERB, Ministry of Environment Forest & Climate
+              Change, UGC and has been a collaborator in several international
+              projects such as MACC (a EU project), PASODOBLE (a EU project),
+              SMOGPROG (Ministry of Environment, Netherlands). He has been a
+              recipient of Young Scientist medal by Society for Science of
+              Climate Change and Sustainable Environment for exemplary work in
+              Environment & Sustainable Development (2023), Uttarakhand
+              Governor’s Award for 2nd best Research Publications in Science
+              (2015-16). He also serves as an editorial board member of the
+              journal SN Applied Sciences, Discover Forests and Journal of
+              Aritifical Intelligence.
             </p>
           </div>
         </div>
       </section>
-
-
       {/* Our Impact */}
       <section className="our-impact">
         <h2>
@@ -226,7 +297,6 @@ const AboutContent = () => {
           </div>
         </div>
       </section>
-
       {/* Achievements */}
       <span className="pinBackground" ref={pinWrapRef}>
         <div className="headd">
@@ -244,7 +314,6 @@ const AboutContent = () => {
           <img src={g1} className="pinImage" alt="" />
         </div>
       </span>
-
       <section className="achievements-section" ref={sectionRef}>
         <div className="timeline-container">
           <div className="timeline-line"></div>
@@ -328,7 +397,6 @@ const AboutContent = () => {
           </div>
         </div>
       </section>
-
       {/* Future Vision */}
       <section className="future-vision">
         <h2>
